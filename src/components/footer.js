06c78f8287cb11636e0d2443a302ce31680d9fb9
amyheyen <39278858+amyheyen@users.noreply.github.com>
2022-03-28 19:03:24 -0400

    Add copyright and update link hover to purple

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,6 +6,7 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
+      &copy; {new Date().getFullYear()} <a href="/">Ash Napier</a> &mdash;
       Built with <a href="https://contentful.com/">Contentful</a> and{' '}
       <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
       <a href="https://github.com/ash-napier/defensive-stance">Source</a>
